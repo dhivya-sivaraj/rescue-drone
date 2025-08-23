ade7cda9dd5b4dbc5c7f257c1fb200cab152a687
dhivya-sivaraj <dhivyasivaraj08@gmail.com>
2025-08-23 21:38:17 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const faders = document.querySelectorAll(".fade");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) entry.target.classList.add("show");
+    });
+  },
+  { threshold: 0.2 }
+);
+
+faders.forEach((f) => observer.observe(f));
